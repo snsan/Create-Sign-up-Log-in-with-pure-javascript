146dd0137008e913af868647b6ecc8f658af3dde
Hnin Nu Shwe <hninnushwe2@gmail.com>
2018-08-19 02:49:13 -0700

    update signup.js
    insert query

--- js/singup.js
+++ js/singup.js
@@ -2,9 +2,24 @@ document.getElementById('btn_signup').onclick = function(){
       var username = document.getElementById('username').value;
       var password = document.getElementById('password').value;
       var c_password = document.getElementById('c_password').value;
-      if(c_password !=  password){
+      if(c_password !=  password && !username){
          alert("Password are not match");
       }else{
-          window.location.href = 'index.html';
+          var user = {
+             username: username,
+             password: password
+          }
+             
+            insertQuery("users",user,function(resp){
+              if(resp.status == 0){
+                window.location.href = 'index.html';                
+              }else{
+                alert("Error occured: ",resp.data);
+                console.log(resp.data);
+              }
+            
+            });
+            
+        //  
       }
 }
