bc8f6bf218eddff06b0a18121b3eaee5fa4c0aed
Hnin Nu Shwe <hninnushwe2@gmail.com>
2018-08-19 02:48:16 -0700

    update signin.js
    select query

--- js/singin.js
+++ js/singin.js
@@ -2,11 +2,14 @@
 document.getElementById('btn_signin').onclick = function(){
       var username = document.getElementById('username').value;
       var password = document.getElementById('password').value;
-      if(username == 'admin' && password == '123'){
-          alert("Login Success");
-          window.location.href = 'success.html';
-      }else{
-         alert("username and password does not exist.");
-      }
+    selectQuery("users","username",username,function(resp){
+          if(resp.status == 0){
+              window.location.href = 'success.html';  
+          }else{
+          
+            alert("username does not exist.");
+          }     
+    }); 
+     
 
 };
